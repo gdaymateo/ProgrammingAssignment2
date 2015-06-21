1d19576afea631cc8a29276f8848ebf698808009
Matt M. <matt.moldthan@gmail.com>
2015-06-21 12:40:46 -0400

    Create makeCacheMatrix.R
    This is also for Prog. Assignment #2

---
+++ makeCacheMatrix.R
@@ -0,0 +1,13 @@
+makeCacheMatrix <- function(x = matrix()) {
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) s <<- solve
+    getinverse <- function() s
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
